ea5ffd7d73b4030c18c13692583453169784efcf
Dave Liu <dave_ubuntu@163.com>
2020-11-01 19:43:27 +0800

    master

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
   
-  controller :session do
-    get 'logon' => :new
-    post 'login' => :create
-    delete 'logout' => :destroy
+    controller :sessions do
+      get  'login' => :new
+      post 'login' => :create
+      delete 'logout' => :destroy
   end
 
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  
-  root 'store#index', as: 'store_index'
+  scope '(:locale)' do
 
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+
+  
+  resources :users
   resources :products do
     get :who_bought, on: :member
   end
